06eb07fa5a4e42288feeae9edb237b90cb7a375f
alejandra-garcias <alejandra.garsanchez@gmail.com>
2023-09-16 17:14:00 +0200

    NavCompleted

--- component/Nav/Nav.jsx
+++ component/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import './Nav.css'
 import Link from 'next/link';
 
@@ -7,16 +7,26 @@ import Link from 'next/link';
 
 
 export default function nav() {
-  
+  const [ScrollY, setScrollY] = useState(0)
+  useEffect(()=>{
+    window.addEventListener('scroll',handleScroll)
+
+  },[])
+  const handleScroll = () =>{
+    let position = window.scrollY
+     setScrollY(position)
+    
+  }
+  console.log(ScrollY)
 
   return (
     <>
     <div className="nav-bar-container">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li ><a href="#about">About</a></li>
-        <li><a href="#certificates">Certificates</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a className={ScrollY<552? 'active': null} href="#home">Home</a></li>
+        <li><a className={ScrollY>=552 && ScrollY<=1000 ? 'active': null}href="#projects">Projects</a></li>
+        <li ><a className={ScrollY>=1000 && ScrollY<=1866 ? 'active': null} href="#about">About</a></li>
+        <li><a className={ScrollY>=1866 && ScrollY<=2129 ? 'active': null} href="#certificates">Certificates</a></li>
+        <li><a className={ScrollY>=2129 && ScrollY<=2298 ? 'active': null} href="#contact">Contact</a></li>
     </div>
 
         
