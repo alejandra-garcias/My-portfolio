d7e737a0b38a9f9fe791371029516b507c53299b
alejandra-garcias <alejandra.garsanchez@gmail.com>
2023-10-17 19:21:06 +0200

    see other projects button

--- app/projects/[id]/page.jsx
+++ app/projects/[id]/page.jsx
@@ -1,63 +1,67 @@
 "use client";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Data from "../../../projects.json";
 import IconGradiant from "@/component/IconMap/IconGradiant";
 import Image from "next/image";
 import "./page.scss";
+import Link from "next/link";
 
-export default function ProjectDetails({params}) {
-    const [project, setProject] = useState(null);
+export default function ProjectDetails({ params }) {
+  const [project, setProject] = useState(null);
 
-    useEffect(() => {
-        const foundProject = Data.find((project) => project.id === params.id);
+  useEffect(() => {
+    const foundProject = Data.find((project) => project.id === params.id);
 
-        if (foundProject) {
-            setProject(foundProject);
-        }
-    }, [params.id]);
-
-    if (!project) {
-        return <div>Proyecto no encontrado</div>;
+    if (foundProject) {
+      setProject(foundProject);
     }
+  }, [params.id]);
 
-    return (
-        <div className="contenedor-page">
-            <div className="contenedor-info">
-                <h1>{project.title}</h1>
-                <Image
-                    src={project.image}
-                    width={2500}
-                    height={500}
-                    alt={project.title}
-                    className={'img-responsive-projects'}
-                />
-                <p>{project.description}</p>
-                <div>
-                    <p>Technologies I used:</p>
-                    <div className="container-iconos">
-                        {project.technologies.map((t, i) => {
-                            return <IconGradiant name={t} key={i}/>;
-                        })}
-                    </div>
-                </div>
-            </div>
+  if (!project) {
+    return <div>Proyecto no encontrado</div>;
+  }
 
-            <div className="contenedor-fotosbotones">
-                <Image
-                    src={project.image}
-                    width={2500}
-                    height={500}
-                    alt={project.title}
-                />
-                <div className="contenedor-botones">
-                    <a href={project.urlGithub}>
-                        <button className="button">Github</button>
-                    </a>
-                    <a href={project.urlSite}>
-                        <button className="button">Go to site</button>
-                    </a>
-                </div>
-            </div>
+  return (
+    <div className="contenedor-page">
+      <div className="contenedor-info">
+        <h1>{project.title}</h1>
+        <Image
+          src={project.image}
+          width={2500}
+          height={500}
+          alt={project.title}
+          className={"img-responsive-projects"}
+        />
+        <p>{project.description}</p>
+        <div>
+          <p>Technologies I used:</p>
+          <div className="container-iconos">
+            {project.technologies.map((t, i) => {
+              return <IconGradiant name={t} key={i} />;
+            })}
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="contenedor-fotosbotones">
+        <Image
+          src={project.image}
+          width={2500}
+          height={500}
+          alt={project.title}
+        />
+        <div className="contenedor-botones">
+          <a href={project.urlGithub}>
+            <button className="button">Github</button>
+          </a>
+          <a href={project.urlSite}>
+            <button className="button">Go to site</button>
+          </a>
+        </div>
+        <Link className="button" href="/#projects">
+            See other projects
+          </Link>
+      </div>
+    </div>
+  );
 }
