e564c77eeda322e91b9a43da724ecc299baf5713
alejandra-garcias <alejandra.garsanchez@gmail.com>
2023-10-18 22:37:36 +0200

    next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = {nextConfig,
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/tu-carpeta-de-despliegue/' : ''}
+module.exports = nextConfig
